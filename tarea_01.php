353f25010cd306891d08bd2119e654d47bc48cbd
Diego A Secas <aspirinetu@gmail.com>
2023-09-06 17:15:57 -0300

    tarea n 01

--- tarea_01.php
+++ tarea_01.php
@@ -34,7 +34,26 @@
             echo "<p>$int_a % $int_b = ";
             echo $int_a % $int_b;
             echo "</p>";
+
+            echo "4)";
+            $gradosCentigrados = 20;
+            $gradosFahrenheit = ($gradosCentigrados * (9 / 5)) + 32;
+            echo "<p>$gradosCentigrados grados centígrados = $gradosFahrenheit grados fahrenheit</p>";
             
+            echo "5)";
+            $rectBase = 18;
+            $rectAltura = 12;
+            $perimetroRect = 2 * $rectAltura + 2 * $rectBase;
+            $areaRect = $rectAltura * $rectBase;
+
+            echo "<p>El perímetro de un rectángulo de base 18 cm y altura 12 cm es de $perimetroRect cm, y su área es de $areaRect cm²</p>";
+            
+            $pi  = M_PI;
+            $circRadio = 30;
+            $perimetroCirc = (2 * $circRadio) * $pi;
+            $areaCirc = $pi * $circRadio ** 2;
+            
+            echo "<p>El perímetro de un círculo de radio 30 cm es de $perimetroCirc cm, y su área es de $areaCirc cm²</p>";
         ?>
     </body>
 </html>
