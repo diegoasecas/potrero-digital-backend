f6fd38cc581d5468d87c9e77d82ceaac9510f427
Diegolas <aspirinetu@gmail.com>
2023-11-23 21:58:25 -0300

    finished login and about fe, begin index fe

--- tp_final_be/login_register_form.php
+++ tp_final_be/login_register_form.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Acceeder</title>
     <!-- TODO: Add any additional head elements (stylesheets, scripts, etc.) -->
 </head>
 <body>
@@ -16,7 +16,7 @@
     <label for="password">Password:</label>
     <input type="password" id="password" name="password" required>
 
-    <input type="submit" value="Login">
+    <input type="submit" value="Acceder">
 </form>
 
 <p>register</p>
